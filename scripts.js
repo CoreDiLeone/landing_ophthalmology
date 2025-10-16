15fb035abd181b279821c0051912df1ee99efa20
CoreDiLeone <fireejuegos@gmail.com>
2025-10-16 20:50:58 -0300

    fixed animation effects

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ window.addEventListener("scroll", () => {
 document.addEventListener("DOMContentLoaded", () => {
   AOS.init({
     offset: 500, // distancia en px desde el borde inferior de la pantalla
-    duration: 800, // duración de la animación
-    once: false, // si quieres que la animación solo ocurra una vez
+    duration: 700, // duración de la animación
+    once: true, // si quieres que la animación solo ocurra una vez
   });
 });
